f8e2002628c09442d539a141d346d26f5034fa68
i.gaev <i.gaev@vkteam.ru>
2026-04-24 16:07:47 +0300

    fix: support implicit TLS (port 465) for SMTP alongside STARTTLS

--- backend/services/identity-service/internal/email/sender.go
+++ backend/services/identity-service/internal/email/sender.go
@@ -1,6 +1,7 @@
 package email
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/smtp"
 	"strings"
@@ -40,8 +41,45 @@ func (sender *Sender) send(toEmail, subject, body string) error {
 
 	msg := buildMessage(sender.from, toEmail, subject, body)
 	addr := sender.host + ":" + sender.port
+
+	// Port 465 uses implicit TLS (SMTPS); other ports use STARTTLS via smtp.SendMail.
+	if sender.port == "465" {
+		return sendSSL(addr, sender.host, sender.from, sender.password, []string{toEmail}, []byte(msg))
+	}
 	auth := smtp.PlainAuth("", sender.from, sender.password, sender.host)
 	return smtp.SendMail(addr, auth, sender.from, []string{toEmail}, []byte(msg))
+}
+
+func sendSSL(addr, host, from, password string, to []string, msg []byte) error {
+	conn, err := tls.Dial("tcp", addr, &tls.Config{ServerName: host})
+	if err != nil {
+		return err
+	}
+	client, err := smtp.NewClient(conn, host)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	auth := smtp.PlainAuth("", from, password, host)
+	if err = client.Auth(auth); err != nil {
+		return err
+	}
+	if err = client.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = client.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := client.Data()
+	if err != nil {
+		return err
+	}
+	if _, err = w.Write(msg); err != nil {
+		return err
+	}
+	return w.Close()
 }
 
 func buildMessage(from, to, subject, body string) string {
